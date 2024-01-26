a4fdeb95c71e6f54aa2c510889e97922acdcdc32
maabfw <maabfw@proton.me>
2024-01-26 11:30:27 +0600

    js first tasks

---
+++ task.js
@@ -0,0 +1,39 @@
+//--------------------task 1 ------------------------- 
+var money = 500;
+var orangesOneKgPrice = 150;
+var appleOneKgPrice = 200;
+var totalCost = orangesOneKgPrice + appleOneKgPrice;
+console.log(totalCost);
+
+var Return = money - totalCost;
+console.log(Return)
+
+
+
+//--------------------task 2 ------------------------- 
+
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var average = (mathematics + biology + chemistry + physics + bangla)/5;
+console.log(average.toFixed(2))
+
+
+//--------------------task 3 -------------------------
+
+
+var guess = 227;
+var divider = 5;
+var remainder = 227%5;
+console.log(remainder)
+
+
+//--------------------task 4 -------------------------
+
+const a = isNaN('11');
+const a2 = isNaN(2-10);
+console.log(a)
+console.log(a2)
